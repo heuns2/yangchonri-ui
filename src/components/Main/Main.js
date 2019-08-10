12bd03625f7c2056ad6d8453d62c0ca7b24619ca
heunji <heunji@cro.com>
2019-08-10 19:01:32 +0900

    동영상

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,65 +1,19 @@
 import React from "react";
-// import Background1 from 'assets/images/bg_1.jpg';
-// import Background2 from 'assets/images/bg_2.jpg';
+import { Container, Row, Col } from "shards-react";
+import ReactPlayer from 'react-player'
 
-class Main extends React.Component {
+export default class Main extends React.Component {
     render(){
-        // const bg1 = {
-        //     backgroundImage: `url(${Background1})`
-        // };
-        // const bg2 = {
-        //     backgroundImage: `url(${Background2}`
-        // };
-
         return (
-            <section className="home-slider owl-carousel">
-                <div className="slider-item">
-                    <div className="container">
-                        <div className="row d-flex slider-text justify-content-center align-items-center" data-scrollax-parent="true">
-
-                            <div className="img"></div>
-                            <div className="text d-flex align-items-center ftco-animate">
-                                <div className="text-2 pb-lg-5 mb-lg-4 px-4 px-md-5">
-                                    <h3 className="subheading mb-3">Featured Posts</h3>
-                                    <h1 className="mb-5">Love the Delicious &amp; Tasty Foods</h1>
-                                    <p className="mb-md-5">A small river named Duden flows by their place and supplies
-                                        it with the necessary regelialia</p>
-                                    <p><a href="#" className="btn btn-black px-3 px-md-4 py-3">Read More
-                                        <span className="icon-arrow_forward ml-lg-4"></span></a>
-                                    </p>
-                                </div>
-                            </div>
-
+            <Container className="dr-example-container">
+                <Row>
+                    <Col lg={{ size: 8, order: 2, offset: 2 }}>
+                        <div>
+                            <ReactPlayer url='https://www.youtube.com/watch?v=iByCYgR9aE8' playing />
                         </div>
-                    </div>
-                </div>
-
-                <div className="slider-item">
-                    <div className="container">
-                        <div className="row d-flex slider-text justify-content-center align-items-center"
-                             data-scrollax-parent="true">
-
-                            <div className="img"></div>
-
-                            <div className="text d-flex align-items-center ftco-animate">
-                                <div className="text-2 pb-lg-5 mb-lg-4 px-4 px-md-5">
-                                    <h3 className="subheading mb-3">Featured Posts</h3>
-                                    <h1 className="mb-5">I am A Blogger &amp; I Love Foods</h1>
-                                    <p className="mb-md-5">A small river named Duden flows by their place and supplies
-                                        it with the necessary regelialia</p>
-                                    <p><a href="#" className="btn btn-black px-3 px-md-4 py-3">Read More
-                                        <span className="icon-arrow_forward ml-lg-4"></span></a>
-                                    </p>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </section>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
-
-
-export default Main;
--- src/components/Nav/Header.js
+++ src/components/Nav/Header.js
@@ -1,22 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import LOGO from "assets/images/logo3.png";
 import "./Header.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 
 import {
     Navbar,
-    NavbarToggler,
     NavbarBrand,
     Nav,
     NavItem,
     NavLink,
-    InputGroup,
-    InputGroupAddon,
-    InputGroupText,
-    FormInput,
-    Collapse
+    Collapse, Container, Row, Col
 } from "shards-react";
 
 
@@ -31,19 +23,26 @@ export default class Header extends React.Component {
         };
 
         return (
-            <Navbar type="light" className="center" expand="md">
-                <NavbarBrand href="/" style={logo}></NavbarBrand>
-                <Collapse navbar>
-                    <Nav navbar className="ml-auto">
-                        <NavItem><NavLink active href="/">HOME</NavLink></NavItem>
-                        <NavItem><NavLink active href="/about">소개</NavLink></NavItem>
-                        <NavItem><NavLink active href="/product">상품</NavLink></NavItem>
-                        <NavItem><NavLink active href="/login">로그인</NavLink></NavItem>
-                        <NavItem><NavLink active href="/myPage">마이페이지</NavLink></NavItem>
-                    </Nav>
+            <Container className="dr-example-container">
+                <Row>
+                    <Col lag={{ size: 8, order: 2, offset: 2 }}>
+                        <Navbar type="light" className="center" expand="md">
+                            <NavbarBrand href="/" style={logo}></NavbarBrand>
+                            <Collapse navbar>
+                                <Nav navbar className="ml-auto">
+                                    <NavItem><NavLink active href="/">HOME</NavLink></NavItem>
+                                    <NavItem><NavLink active href="/about">소개</NavLink></NavItem>
+                                    <NavItem><NavLink active href="/product">상품</NavLink></NavItem>
+                                    <NavItem><NavLink active href="/login">로그인</NavLink></NavItem>
+                                    <NavItem><NavLink active href="/myPage">마이페이지</NavLink></NavItem>
+                                </Nav>
 
-                </Collapse>
-            </Navbar>
+                            </Collapse>
+                        </Navbar>
+                    </Col>
+                </Row>
+            </Container>
+
         );
     }
 }
